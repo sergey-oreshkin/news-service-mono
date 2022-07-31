772d55a7ffa9a6c874cd958045ced66c8a8e5c6e
sergey-oreshkin <joknife79@yandex.ru>
2022-07-31 11:59:56 +0500

    spring init

---
+++ src/main/java/home/serg/newsserviceimpl/NewsServiceImplApplication.java
@@ -0,0 +1,13 @@
+package home.serg.newsserviceimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsServiceImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsServiceImplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/home/serg/newsserviceimpl/NewsServiceImplApplicationTests.java
@@ -0,0 +1,13 @@
+package home.serg.newsserviceimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsServiceImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
